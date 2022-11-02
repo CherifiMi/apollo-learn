d83b9913d159af0524e4ec212a8dbd169cf607fc
CherifiMi <cherifi154949@gmail.com>
2022-11-02 23:44:42 +0100

    useing the data from the grapgql server

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,24 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.api.toJsonString
 import com.example.apollo.LaunchListQuery
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
+import java.awt.SystemColor.text
 
 val apolloClient = ApolloClient.Builder()
     .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com/graphql")
@@ -18,25 +27,29 @@ val apolloClient = ApolloClient.Builder()
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
 
-    rememberCoroutineScope().launch {
+    var result by remember { mutableStateOf<List<LaunchListQuery.Launch>?>(emptyList()) }
+
+    LaunchedEffect(Unit){
         val response = apolloClient.query(LaunchListQuery()).execute()
-
-        println(response.data?.toJsonString())
+        result = response.data?.launches?.launches?.filterNotNull()
     }
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+    LazyColumn (modifier = Modifier.fillMaxSize()){
+        items(result!!){
+            Text(it.id!!)
+            Text(it.site!!)
         }
     }
+
+    println(result)
+
 }
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        App()
+        MaterialTheme {
+            App()
+        }
     }
 }
