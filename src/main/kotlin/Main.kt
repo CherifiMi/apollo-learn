c365d923c10c3783738c68aad554923905cb0c4d
CherifiMi <cherifi154949@gmail.com>
2022-11-02 23:45:34 +0100

    clean up

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,8 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -12,13 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.apollographql.apollo3.ApolloClient
-import com.apollographql.apollo3.api.toJsonString
 import com.example.apollo.LaunchListQuery
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
-import java.awt.SystemColor.text
 
 val apolloClient = ApolloClient.Builder()
     .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com/graphql")
@@ -37,13 +29,9 @@ fun App() {
 
     LazyColumn (modifier = Modifier.fillMaxSize()){
         items(result!!){
-            Text(it.id!!)
             Text(it.site!!)
         }
     }
-
-    println(result)
-
 }
 
 fun main() = application {
