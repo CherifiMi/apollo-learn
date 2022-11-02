9f291e45e045b88353c25c26869fcbbe2be85686
CherifiMi <cherifi154949@gmail.com>
2022-11-02 16:27:21 +0100

    graphgl

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,17 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("multiplatform")
-    id("org.jetbrains.compose")
+    kotlin("jvm") version "1.6.10"
+    id("org.jetbrains.compose") version "1.1.1"
+
+    id("com.apollographql.apollo3").version("3.6.2")
+}
+apollo {
+    packageName.set("com.example.apollo")
 }
 
-group = "com.example"
-version = "1.0-SNAPSHOT"
+group = "org.example"
+version = "1.0"
 
 repositories {
     google()
@@ -16,21 +21,14 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
-kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "11"
-        }
-        withJava()
-    }
-    sourceSets {
-        val jvmMain by getting {
-            dependencies {
-                implementation(compose.desktop.currentOs)
-            }
-        }
-        val jvmTest by getting
-    }
+dependencies {
+    implementation(compose.desktop.currentOs)
+
+    implementation("com.apollographql.apollo3:apollo-runtime:3.6.2")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
 }
 
 compose.desktop {
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.api.toJsonString
+import com.example.apollo.LaunchListQuery
+import kotlinx.coroutines.launch
+
+val apolloClient = ApolloClient.Builder()
+    .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com/graphql")
+    .build()
+
+@Composable
+@Preview
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    rememberCoroutineScope().launch {
+        val response = apolloClient.query(LaunchListQuery()).execute()
+        println(response.data?.toJsonString())
+    }
+
+    MaterialTheme {
+        Button(onClick = {
+            text = "Hello, Desktop!"
+        }) {
+            Text(text)
+        }
+    }
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
+}
